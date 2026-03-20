89a9961345ac60e8343ce9ac3f6702f349d0786b
Marcin Chmylko <chmylkomarcin@gmail.com>
2026-03-20 20:32:00 +0200

    trying CI workflow

--- server/src/assets/assets.module.ts
+++ server/src/assets/assets.module.ts
@@ -3,6 +3,7 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { AssetsController } from './assets.controller';
 import { AssetsService } from './assets.service';
 
+
 @Module({
   imports: [PrismaModule],
   controllers: [AssetsController],
