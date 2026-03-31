8f64122a754e0f57b129d5f61d4a3c62b452256b
Marcin Chmylko <chmylkomarcin@gmail.com>
2026-03-31 23:15:34 +0300

    add frontend url

--- server/src/main.ts
+++ server/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
